7f3325112ed914095c17988c7f390610911a9a4f
nico-bertoli <nicobertoli.1999@gmail.com>
2024-09-18 21:15:36 +0200

    Update InputUtils.h

--- AsciiGameFramework/InputUtils.h
+++ AsciiGameFramework/InputUtils.h
@@ -3,22 +3,20 @@
 
 namespace InputUtils
 {
-	static bool IsAnyKeyPressed();
-	static bool IsPressingSpace() { return GetAsyncKeyState(VK_SPACE) & 0x8000; }
-	static bool IsPressingA() { return GetAsyncKeyState(0x41) & 0x8000; }
-	static bool IsPressingD() { return GetAsyncKeyState(0x44) & 0x8000; }
-	static bool IsPressing1() { return GetAsyncKeyState(0x31) & 0x8000; }
-	static bool IsPressing2() { return GetAsyncKeyState(0x32) & 0x8000; }
-	static bool IsPressing0() { return GetAsyncKeyState(0x30) & 0x8000; }
-	static bool IsPressing9() { return GetAsyncKeyState(0x39) & 0x8000; }
-	static bool IsPressingEsc() { return GetAsyncKeyState(0x1B) & 0x8000; }
-}
+	inline bool IsAnyKeyPressed()
+	{
+		for (int i = 0; i < 256; ++i)
+			if (GetAsyncKeyState(i) & 0x8000)
+				return true;
 
-bool InputUtils::IsAnyKeyPressed()
-{
-	for (int i = 0; i < 256; ++i)
-		if (GetAsyncKeyState(i) & 0x8000)
-			return true;
-
-	return false;
+		return false;
+	}
+	inline bool IsPressingSpace() { return GetAsyncKeyState(VK_SPACE) & 0x8000; }
+	inline bool IsPressingA() { return GetAsyncKeyState(0x41) & 0x8000; }
+	inline bool IsPressingD() { return GetAsyncKeyState(0x44) & 0x8000; }
+	inline bool IsPressing1() { return GetAsyncKeyState(0x31) & 0x8000; }
+	inline bool IsPressing2() { return GetAsyncKeyState(0x32) & 0x8000; }
+	inline bool IsPressing0() { return GetAsyncKeyState(0x30) & 0x8000; }
+	inline bool IsPressing9() { return GetAsyncKeyState(0x39) & 0x8000; }
+	inline bool IsPressingEsc() { return GetAsyncKeyState(0x1B) & 0x8000; }
 }
