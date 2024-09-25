b6d47c3dbd1e1d655a4a75998897dced76fbb258
nico-bertoli <nicobertoli.1999@gmail.com>
2024-09-25 22:58:32 +0200

    alien animated

--- TerminalGameEngine/AlienLowScore.h
+++ TerminalGameEngine/AlienLowScore.h
@@ -1,23 +1,35 @@
 #pragma once
 #include "Alien.h"
+#include "TimeHelper.h"	
 
 class AlienLowScore : public Alien
 {
 private:
-	static const Model MODEL;
+	static const Model MODEL_1;
+	static const Model MODEL_2;
 //------------------------------------------------------ Methods
 public:
 	AlienLowScore(int xPos, int yPos) : Alien(xPos, yPos){}
 	virtual size_t GetScore() const { return 10; }
 
 protected:
-	virtual void InitModel() { SetModel(MODEL); }
+	virtual void InitModel() { SetModel(MODEL_1); }
+
+	virtual void Update()
+	{
+		Alien::Update();
+		SetModel(TimeHelper::Instance().IsTimeForFirstOfTwoModels(0.8) ? MODEL_1 : MODEL_2);
+	}
 };
 
-
-
-const Model AlienLowScore::MODEL =
+const Model AlienLowScore::MODEL_1 =
 {
-	{'a', 'a', 'a', 'a'},
-	{'a', 'a', 'a', 'a'}
+	{'/', '"', '"', '\\'},
+	{'{', '@', '@', '}'}
+};
+
+const Model AlienLowScore::MODEL_2 =
+{
+	{' ', '\\', '/', ' '},
+	{'{', '@', '@', '}'}
 };
