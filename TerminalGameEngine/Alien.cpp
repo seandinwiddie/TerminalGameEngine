8a612769670e81f3a9020ef8623fa94c2f50325c
nico-bertoli <nicobertoli.1999@gmail.com>
2024-09-29 14:10:56 +0200

    aliens blocked by own projectiles fix

--- TerminalGameEngine/Alien.cpp
+++ TerminalGameEngine/Alien.cpp
@@ -22,7 +22,7 @@ void Alien::CALLED_BY_SIM_OnDestroy()
 
 void Alien::Shot()
 {
-	EnemyProjectile* projectile = new EnemyProjectile(GetMidPosX(), GetPosY() -1 , Direction::down, PROJECTILE_SPEED);
+	EnemyProjectile* projectile = new EnemyProjectile(GetMidPosX(), GetPosY() -2 , Direction::down, PROJECTILE_SPEED);
 	Simulation::Instance().TryAddEntity(projectile);
 	//AudioManager::Instance().PlayFx("SpaceInv/shot1.wav");
 }
